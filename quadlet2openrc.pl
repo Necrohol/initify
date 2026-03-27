52959fdc5ad6cecfa2f5f9e2faa1a0cac6fd426d
Michael L. <mike@michaellawrenceit.com>
2026-03-26 23:13:45 -0500

    Update quadlet2openrc.pl

--- quadlet2openrc.pl
+++ quadlet2openrc.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
+# (c) Necrohol, 2026
+# Wrapper to convert podlet/  Podman Quadlets/Services to OpenRC
+# Features: Smart path loading, Privileged container detection.
+
 use strict;
 use warnings;
 use FindBin qw($Bin);
 
 # --- Smart Require Logic ---
 my $found = 0;
-# Check /usr/bin first (installed via ebuild), then local (hot-paste/action)
 foreach my $path ("/usr/bin/initify", "$Bin/initify.pl", "./initify.pl") {
     if (-e $path) {
         require $path;
@@ -21,30 +24,40 @@ foreach my $path ("/usr/bin/cronify", "$Bin/cronify.pl", "./cronify.pl") {
 }
 
 # --- Conversion Logic ---
-my $input = $ARGV[0] or die "Usage: $0 <quadlet.container>\n";
+my $input = $ARGV[0] or die "Usage: $0 <unit_file>\n";
 
-# Note: Quadlets usually need to be pre-processed by 'podman-pull' or 'podman-generate' 
-# to get a real Unit file. Assuming we are targeting the resulting .service:
+# Capture the output from the core initify library
 my $output = "";
 {
     local *STDOUT;
     open(STDOUT, '>', \$output);
-    initify::main($input); # Call the entry point from initify.pl
+    initify::main($input); 
 }
 
-# --- Append OpenRC Fixes ---
+# --- Quadlet/Container Specific Refinements ---
 my @lines = split("\n", $output);
 foreach my $line (@lines) {
-    # 1. Add your backgrounding fix
-    if ($line =~ /^command_args="/) {
-        $line =~ s/"$/ --background --make-pidfile --pidfile \/run\/\${RC_SVCNAME}.pid"/;
+    
+    # 1. Privileged Container Check
+    # Quadlets often lose the --privileged flag during translation if 
+    # the 'privileged=true' was in the [Container] block instead of [Service].
+    if ($input =~ /\.container$/ && $line =~ /^command=/) {
+        if ($line =~ /podman run/ && $line !~ /--privileged/) {
+            $line =~ s/podman run/podman run --privileged/;
+        }
     }
     
-    # 2. Add Privileged check (if initify missed it)
-    if ($input =~ /\.container$/ && $line =~ /command=/) {
-        # Ensure 'podman run' includes --privileged if the quadlet asked for it
-        $line =~ s/podman run/podman run --privileged/ if !($line =~ /--privileged/);
-    }
+    # Note: Backgrounding/PID logic is now handled INSIDE initify.pl
     
     print "$line\n";
 }
+
+exit 0;
+
+__END__
+
+=head1 NAME
+
+quadlet2openrc - Convert Systemd Quadlet units to OpenRC runners.
+
+=cut
